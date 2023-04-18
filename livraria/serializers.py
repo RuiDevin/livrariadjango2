23fab64820a51e3f5eaedbfd863729611dcdbba3
RuiDevin <rafaelalvaro00@gmail.com>
2023-04-18 15:08:48 -0300

    Configuração de Livro Serializer e Livro Detail Serializer

--- livraria/serializers.py
+++ livraria/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework.serializers import ModelSerializer
 
-from livraria.models import Editora, Categoria
+from livraria.models import Editora, Categoria, Livro
 
 class CategoriaSerializer(ModelSerializer):
     class Meta:
@@ -11,3 +11,15 @@ class EditoraSerializer(ModelSerializer):
     class Meta:
         model = Editora
         fields = "__all__"
+
+class LivroSerializer(ModelSerializer):
+    class Meta:
+        model = Livro
+        fields = "__all__"
+        depth = 1
+
+class LivroDetailSerializer(ModelSerializer):
+    class Meta:
+        model = Livro
+        fields = "__all__"
+        depth = 1
--- livraria/views.py
+++ livraria/views.py
@@ -1,7 +1,7 @@
 from rest_framework.viewsets import ModelViewSet
 
-from livraria.models import Categoria, Editora
-from livraria.serializers import CategoriaSerializer, EditoraSerializer
+from livraria.models import Categoria, Editora, Livro
+from livraria.serializers import CategoriaSerializer, EditoraSerializer,LivroDetailSerializer, LivroSerializer
 
 class CategoriaViewSet(ModelViewSet):
     queryset = Categoria.objects.all()
@@ -10,3 +10,10 @@ class CategoriaViewSet(ModelViewSet):
 class EditoraViewSet(ModelViewSet):
     queryset = Editora.objects.all()
     serializer_class = EditoraSerializer
+
+class LivroViewSet(ModelViewSet):
+    queryset = Livro.objects.all()
+    def get_serializzer_class(self):
+        if self.action in ["list", "retrieve"]:
+            return LivroDetailSerializer
+        return LivroSerializer
